6bd31ec08112556ff73639e043acca0081e56716
Bryan <jaguilarsn@gmail.com>
2025-10-20 20:58:47 -0500

    formato url utp

--- servers/utp.py
+++ servers/utp.py
@@ -27,7 +27,7 @@ mcp = FastMCP("mcp-utp", stateless_http=True)
         "type": "object", 
         "properties": {
             "success": {"type": "boolean"},
-            "urls": {"type": "array"}
+            "urls_utp": {"type": "array"}
         }
     }
 )
@@ -39,11 +39,60 @@ def solicitar_urls_informacion_utp(
 
         # Encontrar todos los elementos <loc> y extraer su texto
         loc_values = [loc.get_text(strip=True) for loc in soup.find_all('loc')]
-        print(loc_values)
+
+        utp_urls = {
+            "url_modalidades": {
+                "description": """
+                    URLs de las modalidades de estudio de la UTP.
+
+                    /cgt - Carreras para Gente que Trabaja (semipresencial).
+                    /carreras-a-distancia - Carreras Virtuales a Distancia.
+                    / - Modalidad Presencial y a la vez el Home de UTP.
+                """,
+                "urls": []
+            },
+            "url_carreras_pregrado": {
+                "description": "URLs de las carreras de pregrado de la UTP.",
+                "urls": []
+            },
+            "url_carreras_cgt_semipresencial": {
+                "description": "URLs de las carreras de CGT semipresencial de la UTP.",
+                "urls": []
+            },
+            "url_carreras_virtual_a_distancia": {
+                "description": "URLs de las carreras virtuales a distancia de la UTP.",
+                "urls": []
+            },
+            "url_facultades": {
+                "description": "URLs de las facultades de la UTP.",
+                "urls": []
+            },
+            "url_campus_o_sedes_utp": {
+                "description": "URLs de los campus o sedes de la UTP.",
+                "urls": []
+            }
+        }
+
+        for url in loc_values:
+            if "/pregrado/facultad" in url:
+                utp_urls["url_carreras_pregrado"]["urls"].append(url)
+            elif "/cgt/facultad" in url or "/carreras-para-gente-que-trabaja/facultad" in url:
+                utp_urls["url_carreras_cgt_semipresencial"]["urls"].append(url)
+            elif "/carreras-a-distancia/facultad" in url:
+                utp_urls["url_carreras_virtual_a_distancia"]["urls"].append(url)
+            elif "https://www.utp.edu.pe/" == url or "https://www.utp.edu.pe/cgt" == url or "https://www.utp.edu.pe/carreras-a-distancia" == url:
+                utp_urls["url_modalidades"]["urls"].append(url)
+            elif ".pe/facultad" in url or ".pe/arquitectura" in url:
+                utp_urls["url_facultades"]["urls"].append(url)
+            elif "https://www.utp.edu.pe/pregrado" == url or "https://www.utp.edu.pe/pregrado/ab-testing" == url:
+                pass
+            else:
+                utp_urls["url_campus_o_sedes_utp"]["urls"].append(url)
+
         return {
             "success": True,
             "message": "URLs UTP obtenidas correctamente.",
-            "urls": loc_values
+            "urls": utp_urls
         }
     except Exception as e:
         print(f"Error: {e}")
@@ -158,4 +207,4 @@ def clean_html(html):
     return soup.prettify()
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8004, path="/mcp")
+    mcp.run(transport="http", host="0.0.0.0", port=8104, path="/mcp")
